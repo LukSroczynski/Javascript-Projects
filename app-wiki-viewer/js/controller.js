4479cc558321feecec25b8e944b5d9219b7794a5
Surreallistic <iluzion5@o2.pl>
2017-02-17 14:54:45 +0100

    50% done

--- app-wiki-viewer/js/controller.js
+++ app-wiki-viewer/js/controller.js
@@ -1,14 +1,34 @@
+"use strict";
 /**
  * Created by Surreallistic on 13.02.2017.
  */
-"use strict";
 
-
+let searchText;
 
 $( document ).ready(function() {
 
-    $('.search-button').click(function(){
-        $(this).parent().toggleClass('open');
-    });
+    getAnimationForSearchIcon();
+    setSearchText();
 
 });
+
+function getAnimationForSearchIcon() {
+    $('.search-button').click(function(){
+        $(this).parent().toggleClass('open');
+        console.log();
+    });
+}
+
+function setSearchText() {
+    $(document).keypress(function(e) {
+        if(e.which == 13) {
+            searchText = $("#search-field").val();
+            populateSearchResult();
+        }
+    });
+}
+
+function populateSearchResult() {
+    // $(".conteiner").append("<div class=" + "div-radius" + ">hello world</div>");
+    $(".conteiner").append("<div class=\"div-radius\">hello world</div>");
+}
